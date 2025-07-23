35986777b04ff4ecb8183d7ab2225fe8c94b30fa
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 20:38:49 +0200

    updated

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -6,3 +6,12 @@ export interface PostProps {
     body: string;
     name: string;
 }
+// interfaces/index.ts
+
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  avatar: string;
+  role: 'admin' | 'user' | 'guest'; // You can adjust based on roles used
+}
