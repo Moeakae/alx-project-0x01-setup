6d6cdf575fcb2dbec4bdeb3a3c9b0b8a9b298e7d
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 22:03:43 +0200

    updated

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,13 +2,11 @@
 
 import UserCard from '@/components/common/UserCard';
 import { UserProps } from '@/interfaces';
-import { GetServerSideProps } from 'next';
 
-interface UsersPageProps {
-  users: UserProps[];
-}
-
-const Users = ({ users }: UsersPageProps) => {
+const users: UserProps[] = [
+  
+];
+const Users = () => {
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {users.map((user) => (
@@ -19,16 +17,3 @@ const Users = ({ users }: UsersPageProps) => {
 };
 
 export default Users;
-
-// ✅ Mock API fetch
-export const getServerSideProps: GetServerSideProps = async () => {
-  const users: UserProps[] = [
-    
-    
-  ];
-
-  return {
-    props: { users },
-  };
-};
-
