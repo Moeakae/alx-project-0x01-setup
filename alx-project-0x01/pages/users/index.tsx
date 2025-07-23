0f3b4aa5d3af6f0f7f373ce29e99eaca344e2264
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 23:01:21 +0200

    updated

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -3,29 +3,39 @@
 import UserCard from '@/components/common/UserCard';
 import { UserProps } from '@/interfaces';
 
-const users: UserProps[] = [
-  
-];
-const Users = () => {
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const Users = ({ posts }: UsersPageProps) => {
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      {users.map((user) => (
-        <UserCard key={user.id} {...user} />
+      {posts.map((post) => (
+        <UserCard key={post.id} {...post} />
       ))}
     </div>
-  )
-}
-    export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  );
+};
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  
+  const posts: UserProps[] = data.map((user: any) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    avatar: `https://i.pravatar.cc/150?u=${user.id}`, 
+    role: 'user', 
+  }));
 
   return {
     props: {
-      posts
-    }
-  }
-}
+      posts,
+    },
+  };
+};
 
-  
+export default Users;
 
- export default Users;
