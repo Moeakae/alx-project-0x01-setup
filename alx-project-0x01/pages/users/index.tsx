09c1ec69ab3e5e2a60133c193f1979e63bf5e5a5
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 20:23:32 +0200

    updated

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -7,6 +7,17 @@ const Home: React.FC = () => {
             
         </div>
     );
-};
+}
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default Home;
