f83e20f0eca09e39b25bff06be85d23b6f98bb7f
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 23:20:21 +0200

    updated

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -17,7 +17,11 @@ const UserCard: React.FC<UserProps> = ({ name, email, avatar, role }) => {
           <p className="text-sm text-gray-500">{email}</p>
           <span className="text-xs mt-1 inline-block bg-indigo-100 text-indigo-700 px-2 py-0.5 rounded-full capitalize">
             {role}
+            
+
           </span>
+          
+
         </div>
       </div>
     </div>
