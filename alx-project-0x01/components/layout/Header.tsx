dd6cb80800ea2c388a85a62c93d9ce139ff9da52
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-07-23 19:16:13 +0200

    updated

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header style={{ padding: '1rem', background: '#f5f5f5', borderBottom: '1px solid #ddd' }}>
+            <h1>My Project Header</h1>
+        </header>
+    );
+};
+
+export default Header;
